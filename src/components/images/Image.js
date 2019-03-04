2e3e0c9e41076fd904f781ac4bbe3d46ba53f3ab
Igor Savin <beixiaofeng@gmail.com>
2019-03-04 16:06:29 +0200

    last commit

--- src/components/images/Image.js
+++ src/components/images/Image.js
@@ -9,11 +9,13 @@ const Image = props => {
       <div className="card mb-4 shadow-sm">
       <div className="card-body">
           <img src={image.webImage.url} className="card-img" alt="..." />
+          <div className="description" >
           <h5 className="card-title">{image.title}</h5>
         <p className="card-text"><i className="fas fa-user"></i> {image.principalOrFirstMaker}</p>
         <Link to={`/details/image/${image.objectNumber}`} className="btn btn-dark">
               View more...
             </Link>
+            </div>
       </div>
       </div>
     </div>
